6c36828b4974b2993ba8f4c0f3b99ae67062da91
timanshu <timanshujawa@gmail.com>
2020-12-15 17:10:03 +0530

    Added CarNotFoundException Class

---
+++ motorworld/src/main/java/com/motorworld/cars/exception/CarNotFoundException.java
@@ -0,0 +1,14 @@
+package com.motorworld.cars.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class CarNotFoundException extends RuntimeException{
+	
+	public CarNotFoundException(String message) 
+	{
+        super(message);
+    }
+
+}
