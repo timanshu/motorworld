3108d5f40b1b2b8cc225ed4caf3089cb9f307615
timanshu <timanshujawa@gmail.com>
2023-07-27 13:26:41 +0530

    Revert "[#3]: Adding Bike Service"

--- motorworld/src/main/java/com/motorworld/cars/data/CarsRepository.java
+++ motorworld/src/main/java/com/motorworld/cars/data/CarsRepository.java
@@ -1,11 +1,9 @@
 package com.motorworld.cars.data;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.motorworld.cars.model.Car;
 
-@Repository
 public interface CarsRepository extends CrudRepository<Car, Integer>
 {
 
